9f6e423677189a6a7a887a0ca0a4857456799efc
Andy Wang <Andy.HF.Wang@gmail.com>
2018-02-01 00:02:45 -0500

    Started adding animations to envelope.

---
+++ portfolio.js
@@ -0,0 +1,63 @@
+function test() {
+    document.getElementById("envelopebar").innerHTML = "testing";
+}
+
+function envelopeButtonClick() {
+    var top = document.getElementById("envelopetop");
+    var bot = document.getElementById("envelopebottom");
+    var button = document.getElementById("openbutton");
+    var posTop = 0;
+    var posButton = 35;
+    var posBot = 40;
+    var id = setInterval(frame, 20);
+    function frame() {
+        if (posTop == -40 && posBot == 100) {
+            top.style.minHeight = "0%";
+            bot.style.minHeight = "0%";
+            button.onclick = test;
+            clearInterval(id);
+        } else {
+            posTop -= 1;
+            posButton -= 1;
+            posBot += 1.5;
+            top.style.top = posTop + '%';
+            button.style.top = posButton + '%';
+            bot.style.top = posBot + '%';
+        }
+    }
+}
+
+$("#openbutton").bind("animationEnd", function () {
+    $(this).removeClass("animated");
+})
+
+$("#openbutton").hover(function () {
+    $(this).addClass("animated");
+})
+
+/*function buttonSpin(x) {
+    var button = document.getElementById("openbutton");
+    var angle = button.innerHTML = parseInt(button.getPropertyValue("transform"));
+    var angle = 45;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (x == 1) {
+            clearInterval(id);
+        } else {
+            angle++;
+            button.style.transform = "rotate(" + angle + "deg)";
+        }
+    }
+}*/
+
+/*$("#openbutton").mouseenter(function () {
+    $(this).addClass("change").delay(5000).queue(function (){
+        $(this).removeClass("change").dequeue();
+    });
+});/*
+
+/*function attachHandlers() {
+    document.getElementById("openbutton").onclick = envelopeButtonClick;
+}
+
+window.onload = attachHandlers;*/
